067d76fe4b7f63201350a9cfb87acb91902a13ad
vengateshm <40466166+vengateshm@users.noreply.github.com>
2022-09-16 10:33:04 +0530

    exposed rest endpoint for order creation

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const Razorpay = require('razorpay');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 var instance = new Razorpay({
     key_id: 'rzp_test_zoXB9JxruFIFPV',
     key_secret: '43kh2ez219Ow0FXb4WXXU1ai',
   });
 
-var options = {
-  amount: 50000,  // amount in the smallest currency unit
-  currency: "INR",
-  receipt: "order_rcptid_11"
-};
-instance.orders.create(options, function(err, order) {
-  console.log(order);
+app.post('/orders', (req, res) => {
+    var options = {
+        amount: 50000,  // amount in the smallest currency unit
+        currency: "INR",
+        receipt: "order_rcptid_11"
+      };
+      instance.orders.create(options, function(err, order) {
+        console.log(order);
+        res.send(order);
+      });
 });
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
